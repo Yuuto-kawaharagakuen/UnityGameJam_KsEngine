021ee2e6ad9e10146a67976729908b5e4fe7d8e9
siro-Git-Hub627628 <yuutokasai6@icloud.com>
2026-07-28 19:27:53 +0900

    タイトル/ゲームオーバー/ゲームクリアでマウスクリック後にゲームパッド操作ができなくなる不具合の修正

--- Assets/Scripts/GamepadButtonTrigger.cs
+++ Assets/Scripts/GamepadButtonTrigger.cs
@@ -3,13 +3,13 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
-// このスクリプトが付いたButtonにアタッチする
-// マウスでのクリックは無効化し、ゲームパッドのAボタン(South)は
-// 「現在選択中のボタン」の時だけOnClickを実行する
 [RequireComponent(typeof(Button))]
 public class GamepadButtonTrigger : MonoBehaviour, IPointerClickHandler
 {
     private Button button;
+
+    // 直前に選択されていたボタンを覚えておく(複数のGamepadButtonTriggerで共有)
+    private static GameObject lastSelected;
 
     void Awake()
     {
@@ -21,8 +21,7 @@ public class GamepadButtonTrigger : MonoBehaviour, IPointerClickHandler
         var gamepad = Gamepad.current;
         if (gamepad == null) return;
 
-        // 自分が今EventSystem上で選択されているボタンでなければ何もしない
-        //if (EventSystem.current.currentSelectedGameObject != gameObject) return;
+        if (EventSystem.current.currentSelectedGameObject != gameObject) return;
 
         if (gamepad.buttonSouth.wasPressedThisFrame)
         {
@@ -30,7 +29,20 @@ public class GamepadButtonTrigger : MonoBehaviour, IPointerClickHandler
         }
     }
 
-    // マウスでクリックされた時に呼ばれるイベントを、あえて何もしないことで無効化する
+    void LateUpdate()
+    {
+        // 現在選択されているものがあれば覚えておく
+        if (EventSystem.current.currentSelectedGameObject != null)
+        {
+            lastSelected = EventSystem.current.currentSelectedGameObject;
+        }
+        // マウスクリックなどで選択が外れてしまった場合、直前のボタンへ選択を戻す
+        else if (lastSelected != null && lastSelected.activeInHierarchy)
+        {
+            EventSystem.current.SetSelectedGameObject(lastSelected);
+        }
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         // 何もしない = マウスクリックを無視
